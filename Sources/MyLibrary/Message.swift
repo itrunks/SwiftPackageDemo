d0b0708ea3f7367915b63e91a1d0747f5b1046f6
Raja Pitchai <raja.pitchai@grootan.com>
2019-12-15 21:04:01 +0530

    Message Class added

---
+++ Sources/MyLibrary/Message.swift
@@ -0,0 +1,15 @@
+//
+//  File.swift
+//  
+//
+//  Created by Raja on 15/12/19.
+//
+
+import Foundation
+
+struct Message {
+    
+    static func welcome() -> String{
+        return "Hello World"
+    }
+}
